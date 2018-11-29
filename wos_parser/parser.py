f9c8da8bcaf03992af75a4adc8946e3a77e441a9
SultanOrazbayev <contact@econpoint.com>
2018-11-29 14:44:02 -0500

    Update parser.py

--- wos_parser/parser.py
+++ wos_parser/parser.py
@@ -90,7 +90,7 @@ def extract_authors(elem):
     authors = list()
     names = elem.findall('./static_data/summary/names/')
     for name in names:
-        dais_id = name.attrib.get('dais_id', '')
+        dais_id = name.attrib.get('daisng_id', '')
         seq_no = name.attrib.get('seq_no', '')
         role = name.attrib.get('role', '')
         addr_no = name.attrib.get('addr_no', '')
@@ -106,7 +106,7 @@ def extract_authors(elem):
             last_name = name.find('last_name').text
         else:
             last_name = ''
-        author = {'dais_id': daisng_id,
+        author = {'dais_id': dais_id,
                   'seq_no': seq_no,
                   'addr_no': addr_no,
                   'role': role,
